724c5b38c3f44e99da421a9cda3ef4ff339e342a
German Fica <germafica@gmail.com>
2022-03-02 17:52:57 -0300

    Add hibernate configuration

--- src/main/java/com/germanfica/TestJdbc.java
+++ src/main/java/com/germanfica/TestJdbc.java
@@ -1,14 +1,27 @@
 package com.germanfica;
 
+import com.germanfica.entity.User;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 import java.sql.*;
 
 public class TestJdbc {
+	//https://logging.apache.org/log4j/2.x/manual/migration.html
+	private static final Logger log = LogManager.getLogger(TestJdbc.class);
+
 	public static void main( String[] args) throws SQLException, ConfigurationException {
 		// == application.properties  ==
 		FileBasedConfigurationBuilder<PropertiesConfiguration> builder =
@@ -44,5 +57,63 @@ public class TestJdbc {
 
 		myConn.close();
 		System.out.println("connection closed. :)");
+
+		// == hibernate  ==
+		log.info("Now let's use hibernate!");
+
+		// https://www.programcreek.com/java-api-examples/?api=org.hibernate.boot.registry.StandardServiceRegistryBuilder
+		// https://www.programcreek.com/java-api-examples/?code=robeio%2Frobe%2Frobe-master%2Frobe-hibernate%2Fsrc%2Ftest%2Fjava%2Fio%2Frobe%2Fhibernate%2FHibernateUtil.java
+		Configuration configuration = new Configuration()
+				.setProperty("hibernate.dialect", config.getString("spring.jpa.database-platform"))
+				.setProperty("hibernate.connection.driver_class", config.getString("datasource.driverClassName"))
+				.setProperty("hibernate.connection.url", config.getString("datasource.url"))
+				.setProperty("hibernate.connection.username", config.getString("datasource.username"))
+				.setProperty("hibernate.connection.password", config.getString("datasource.password"))
+				.addClass(User.class);
+
+		// read configuration and build session factory
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
+
+		SessionFactory sessionFactory = null;
+
+		try {
+			sessionFactory = new MetadataSources(registry)
+					.buildMetadata()
+					.buildSessionFactory();
+		}
+		catch (Exception e) {
+			StandardServiceRegistryBuilder.destroy(registry);
+			log.error("cannot create sessionFactory", e);
+			System.exit(1);
+		}
+
+		// create session, open transaction and save test entity to db
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+
+		try {
+			User testEntity = new User();
+			testEntity.setUsername("albedo");
+			testEntity.setEmail("albedo@localhost");
+			testEntity.setFirstName("albedo");
+			testEntity.setLastName("h.");
+			testEntity.setPassword("12345678");
+
+			log.info("user: " + testEntity.getFirstName(), testEntity);
+
+			//session.persist(testEntity);
+			tx.commit();
+		}
+		catch (Exception e) {
+			tx.rollback();
+			log.error("cannot commit transaction", e);
+		}
+		finally {
+			session.close();
+		}
+
+		// clean up
+		sessionFactory.close();
 	}
 }
