fb11fd8fb243c774e5f10f6ae324354280f33f2b
German Fica <germafica@gmail.com>
2022-03-02 17:02:13 -0300

    Add User entity

---
+++ src/main/java/com/germanfica/entity/User.java
@@ -0,0 +1,43 @@
+package com.germanfica.entity;
+
+import java.time.LocalDate;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter(AccessLevel.PUBLIC)
+@Getter(AccessLevel.PUBLIC)
+@RequiredArgsConstructor
+//@NoArgsConstructor // JPA requires that this constructor be defined as public or protected
+@Entity(name="User")
+@Table(name="user")
+public class User  {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+
+	@Column(name="email")
+	private String email;
+
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="birth_date")
+	private LocalDate birthDate;
+
+	private String city;
+
+	private String province;
+
+	private String address;
+}
