f2d5ae5913780bd8d6aec1de22c5a9b98074af0f
German Fica <germafica@gmail.com>
2022-03-02 10:15:42 -0300

    Add maven project

---
+++ src/main/java/com/germanfica/userstoragespi/UserStorageSpiApplication.java
@@ -0,0 +1,13 @@
+package com.germanfica.userstoragespi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStorageSpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserStorageSpiApplication.class, args);
+	}
+
+}
