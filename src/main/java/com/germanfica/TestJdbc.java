8e28335799c1831c0d92c49d901cc9e8fad96115
German Fica <germafica@gmail.com>
2022-03-03 15:53:28 -0300

    Update TestJdbc

--- src/main/java/com/germanfica/TestJdbc.java
+++ src/main/java/com/germanfica/TestJdbc.java
@@ -7,6 +7,8 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.HibernateException;
 
 import java.sql.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class TestJdbc {
 	//https://logging.apache.org/log4j/2.x/manual/migration.html
@@ -25,7 +27,42 @@ public class TestJdbc {
 		testEntity.setLastName("h.");
 		testEntity.setPassword("12345678");
 
-		userRepository.save(testEntity);
+		User delUser = new User();
+		delUser.setId(35);
+
+		Set<Integer> ids = new LinkedHashSet();
+		ids.add(1);
+		ids.add(4);
+
+		Set<User> users = new LinkedHashSet();
+		User user1 = new User();
+		testEntity.setUsername("albedo16");
+		testEntity.setEmail("albedo@localhost");
+		testEntity.setFirstName("albedo");
+		testEntity.setLastName("h.");
+		testEntity.setPassword("12345678");
+		User user2 = new User();
+		testEntity.setUsername("albedo17");
+		testEntity.setEmail("albedo@localhost");
+		testEntity.setFirstName("albedo");
+		testEntity.setLastName("h.");
+		testEntity.setPassword("12345678");
+
+		//userRepository.save(testEntity);
 		//userRepository.findById(1);
+		//userRepository.delete(delUser);
+		//log.info(userRepository.findById(90).get().getFirstName());
+		//log.info("Id del username: "+userRepository.findByUsername("guillermina").get().getId());
+		//userRepository.findAll().forEach(user -> log.info("username: " + user.getUsername()));
+		//userRepository.deleteById(17);
+		//log.info(userRepository.count());
+		//userRepository.findAllById(ids).forEach(user -> log.info("Username: " + user.getUsername()));
+		//log.info("exists? " + userRepository.existsById(4));
+		
+		// Not implemented
+		userRepository.saveAll(users);
+		userRepository.deleteAllById(ids);
+		userRepository.deleteAll(users);
+		userRepository.deleteAll();
 	}
 }
